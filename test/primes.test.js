0cc591a0f6626e3e28dd297b54b9f7ff1ab56832
Rachel Wong <rachelwong4444@gmail.com>
2026-09-15 18:49:44 +0100

    test: add tests for primes that are generated

---
+++ test/primes.test.js
@@ -0,0 +1,30 @@
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
+import { generatePrimes } from "../src/primes.js";
+
+describe("generatePrimes", () => {
+  it("returns the first prime", () => {
+    assert.deepEqual(generatePrimes(1), [2]);
+  });
+
+  it("returns the first 3 primes", () => {
+    assert.deepEqual(generatePrimes(3), [2, 3, 5]);
+  });
+
+  it("returns the first 10 primes", () => {
+    assert.deepEqual(generatePrimes(10), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+  });
+
+  it("throws an error for invalid input", () => {
+    assert.throws(() => generatePrimes(0));
+    assert.throws(() => generatePrimes(-1));
+    assert.throws(() => generatePrimes(1.5));
+  });
+
+  it("works with a larger number of primes", () => {
+    const primes = generatePrimes(1000);
+
+    assert.equal(primes.length, 1000);
+    assert.equal(primes[999], 7919);
+  });
+});
